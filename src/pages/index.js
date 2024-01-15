b1d818ffbbc023b5716916320dc94f33dfebfb37
iduk <iduk.design@gmail.com>
2024-01-15 20:17:23 +0900

    내용추가

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,6 +19,12 @@ import Netflix from '../assets/images/logo_netflix.svg'
 import Oracle from '../assets/images/logo_oracle.svg'
 import RevealContainer from '../components/revealContainer'
 import OpenfloorText from '../assets/images/openfloor-text.svg'
+import ImagePng from '../assets/images/3d_render.png'
+import SHBank from '../assets/images/shbank.png'
+import OliveEZ from '../assets/images/olive-ez.png'
+import OliveLounge from '../assets/images/olive-lounge.png'
+import SBIBank from '../assets/images/sidar-bank.png'
+import OlivePDA from '../assets/images/olive-pda.png'
 import * as styles from './index.module.scss'
 import classNames from 'classnames/bind'
 import ParallaxText from '../components/parallaxText'
@@ -63,107 +69,101 @@ const skills = [
 const histories = [
   {
     date: '25 Mar 2022',
-    title: '올영 EZ (UX/UI/FRONT/MAINTEANENCE)',
-    partner: '(주)CJ올리브영',
-    solution: 'LMS',
-    src: 'https://images.unsplash.com/photo-1480455624313-e29b44bbfde1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3270&q=80',
+    title: '올영 EZ',
+    partner: '(주)CJ올리브영',  
+    solution: 'UX/UI/FRONT/MAINTEANENCE',
+    src: OliveEZ,
   },
   {
     date: '22 Aug 2022',
-    title: '올리브 라운지 (UX/UI/FRONT/MAINTEANENCE)',
+    title: '올리브 라운지',
     partner: '(주)CJ올리브영',
-    solution: 'LMS',
-    src: 'https://images.unsplash.com/photo-1704226769822-026a349945ba?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNXx8fGVufDB8fHx8fA%3D%3D',
+    solution: 'UX/UI/FRONT/MAINTEANENCE',
+    src: OliveLounge,
   },
   {
     date: '04 Sept 2022',
-    title: '셀프 체크아웃 (DESIGN)',
+    title: '셀프 체크아웃',
     partner: '(주)CJ올리브영',
-    solution: 'LMS',
-    src: 'https://images.unsplash.com/photo-1704226769822-026a349945ba?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNXx8fGVufDB8fHx8fA%3D%3D',
+    solution: 'DESIGN',
   },
   {
     date: '03 Feb 2022',
-    title: 'PDA 2.0 (UX/UI/FRONT/BACKEND/MAINTEANENCE)',
+    title: 'SMART PDA 2.0',
     partner: '(주)CJ올리브영',
-    solution: 'LMS',
-    src: 'https://images.unsplash.com/photo-1704226769822-026a349945ba?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNXx8fGVufDB8fHx8fA%3D%3D',
+    solution: 'UX/UI/FRONT/BACKEND/MAINTEANENCE',
+    src: OlivePDA,
   },
   {
     date: '30 Jan 2022',
-    title: 'SBI 저축은행 개인 뱅킹 (UX/UI/FRONT)',
+    title: 'SBI 저축은행 개인 뱅킹',
     partner: 'SBI저축은행',
-    solution: 'LMS',
-    src: 'https://images.unsplash.com/photo-1704226769822-026a349945ba?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNXx8fGVufDB8fHx8fA%3D%3D',
+    solution: 'UX/UI/FRONT',
+    src: SBIBank,
   },
   {
     date: '04 Sept 2022',
-    title: '하나 손해 보험 고도화 (FRONT/BACKEND)',
+    title: '하나손해보험 시스템 고도화',
     partner: '하나손해보험',
-    solution: 'CRM',
-    src: 'https://images.unsplash.com/photo-1704226769822-026a349945ba?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNXx8fGVufDB8fHx8fA%3D%3D',
+    solution: 'FRONT/BACKEND',
   },
   {
     date: '30 Jan 2022',
-    title: '광주은행 개인 뱅킹 / 기업 뱅킹 (FRONT/BACKEND)',
+    title: '광주은행 개인뱅킹/기업뱅킹',
     partner: '광주은행',
-    solution: 'LMS',
-    src: 'https://images.unsplash.com/photo-1704226769822-026a349945ba?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNXx8fGVufDB8fHx8fA%3D%3D',
+    solution: 'FRONT/BACKEND',
   },
   {
     date: '04 Sept 2022',
-    title: '세탁 O2O 플랫폼 구축 (APP / DESKTOP / WEB / SERVER)',
+    title: '세탁 O2O 서비스 플랫폼',
     partner: '온디맨드랩',
-    solution: 'CRM',
-    src: 'https://images.unsplash.com/photo-1704226769822-026a349945ba?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNXx8fGVufDB8fHx8fA%3D%3D',
+    solution: 'APP/DESKTOP/WEB/SERVER',
   },
   {
     date: '04 Sept 2022',
-    title: '배달 O2O 플랫폼 구축 (APP / DESKTOP / WEB / SERVER)',
+    title: '배달 O2O 서비스 플랫폼',
     partner: '온디맨드랩',
-    solution: 'CRM',
-    src: 'https://images.unsplash.com/photo-1704226769822-026a349945ba?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNXx8fGVufDB8fHx8fA%3D%3D',
+    solution: 'APP/DESKTOP/WEB/SERVER',
   },
   {
     date: '30 Jan 2022',
-    title: 'SPC 잠바주스 키오스크 플랫폼 구축 (APP / WEB / SERVER)',
+    title: 'SPC 잠바주스 키오스크 서비스 플랫폼',
     partner: 'SPC',
-    solution: 'APP',
-    src: 'https://images.unsplash.com/photo-1704226769822-026a349945ba?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNXx8fGVufDB8fHx8fA%3D%3D',
+    solution: 'APP/WEB/SERVER',
   },
   {
     date: '04 Sept 2022',
-    title: '삼성중공업 모바일 무도면 시스템 (APP / SERVER)',
+    title: '삼성중공업 모바일 무도면 시스템',
     partner: '삼성중공업',
-    solution: 'CRM',
-    src: 'https://images.unsplash.com/photo-1704226769822-026a349945ba?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNXx8fGVufDB8fHx8fA%3D%3D',
+    solution: 'APP/SERVER',
   },
 ]
 const projectList = [
   {
     date: '25 Mar 2022',
-    title: 'Design & Human Interface',
+    title: '올리브영 매장업무시스템',
+    desc: 'OLIVEYOUNG',
+    src: OliveEZ
   },
   {
     date: '22 Aug 2022',
-    title: 'Website admin page redesign',
+    title: '올리브영 교육센터',
+    desc: 'OLIVEYOUNG',
+    src: OliveLounge
   },
   {
     date: '04 Sept 2022',
-    title: 'Business Platform debt, docs, refactors and stability',
+    title: '신협 기업 뱅킹',
+    desc: 'shinhyup bank',
+    src: SHBank
   },
   {
     date: '03 Feb 2022',
-    title: 'Web UI kit automation',
+    title: 'SBI저축은행 사이다뱅크',
+    desc: 'SBI Bank',
+    src: SBIBank
   },
-  {
-    date: '30 Jan 2022',
-    title: 'Business Platform debt, docs, refactors and stability',
-  },
-  {
-    date: '04 Sept 2021',
-    title: 'Refactors and stability',
-  },
+  
 ]
 
 const partners = [
@@ -216,284 +216,35 @@ function HistoriesList({ list }) {
           setIsActive(false)
         }}
       >
-        <motion.div
-          variants={anim}
-          animate={isActive ? 'open' : 'closed'}
-          className={cx('imgContainer')}
-        >
-          <img src={src} />
-        </motion.div>
-        <p className="my-2 my-lg-0 h3" style={{ display: 'inline-block' }}>
-          {title}
-        </p>
+        <div className='my-2 my-lg-0 h3'>
+          <p className='pr-2' style={{ display: 'inline-block' }}>
+            {title}
+          </p>
+          {src ? 
+            <motion.div
+              variants={anim}
+              animate={isActive ? 'open' : 'closed'}
+              className={cx('imgContainer')}
+            >
+              <img src={src} alt={src} />
+            </motion.div>
+            : ''
+          }
+          <p style={{ display: 'inline-block' }}>{solution}</p>
+        </div>
       </div>
       <div className="col-lg-2 col-6">{partner}</div>
-      <div className="col-lg-2 col-6">{solution}</div>
+      
     </Link>
   )
 }
 
 const IndexPage = () => {
-  // const ref = useRef(null)
-
-  // const { scrollYProgress } = useScroll({
-  //   target: ref,
-  //   offset: ['end end', 'start start'],
-  // })
-
   return (
     <Layout className={cx('index')}>
       <section
         className={cx('fluid', 'stage', 'justify-center', 'text-center')}
       >
-        {/* <motion.div
-          animate={{ rotateY: 360 }}
-          transition={{ ease: 'linear', duration: 7, repeat: Infinity }}
-          className={cx('motionTrack')}
-        >
-          <motion.div
-            animate={{
-              rotate: 360,
-            }}
-            transition={{ ease: 'linear', duration: 15, repeat: Infinity }}
-            className={cx('typoCircle')}
-          >
-            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 382.34 382.35">
-              <text
-                className={cx('typo')}
-                transform="translate(148.5 46.29) rotate(-9.76)"
-              >
-                <tspan x={0} y={0}>
-                  T
-                </tspan>
-              </text>
-              <text
-                className={cx('typo')}
-                transform="translate(179.9 40.61) rotate(2.27)"
-              >
-                <tspan x={0} y={0}>
-                  E
-                </tspan>
-              </text>
-              <text
-                className={cx('typo')}
-                transform="translate(211.72 41.4) rotate(14.86)"
-              >
-                <tspan x={0} y={0}>
-                  A
-                </tspan>
-              </text>
-              <text
-                className={cx('typo')}
-                transform="translate(245.16 49.36) rotate(29.48)"
-              >
-                <tspan x={0} y={0}>
-                  M
-                </tspan>
-              </text>
-              <text
-                className={cx('typo')}
-                transform="translate(281.66 71.26) rotate(40.12)"
-              >
-                <tspan x={0} y={0}>
-                  {' '}
-                </tspan>
-              </text>
-              <text
-                className={cx('typo')}
-                transform="translate(292.84 79.1) rotate(49.77)"
-              >
-                <tspan x={0} y={0}>
-                  O
-                </tspan>
-              </text>
-              <text
-                className={cx('typo')}
-                transform="translate(316.8 107.37) rotate(62.84)"
-              >
-                <tspan x={0} y={0}>
-                  P
-                </tspan>
-              </text>
-              <text
-                className={cx('typo')}
-                transform="translate(331.63 135.6) rotate(75.04)"
-              >
-                <tspan x={0} y={0}>
-                  E
-                </tspan>
-              </text>
-              <text
-                className={cx('typo')}
-                transform="translate(340.44 166.18) rotate(88.12)"
-              >
-                <tspan x={0} y={0}>
-                  N
-                </tspan>
-              </text>
-              <text
-                className={cx('typo')}
-                transform="translate(341.55 203.26) rotate(100.62)"
-              >
-                <tspan x={0} y={0}>
-                  F
-                </tspan>
-              </text>
-              <text
-                className={cx('typo')}
-                transform="translate(336.41 232.15) rotate(111.86)"
-              >
-                <tspan x={0} y={0}>
-                  L
-                </tspan>
-              </text>
-              <text
-                className={cx('typo')}
-                transform="translate(326.57 258.83) rotate(124.19)"
-              >
-                <tspan x={0} y={0}>
-                  O
-                </tspan>
-              </text>
-              <text
-                className={cx('typo')}
-                transform="translate(306.08 289.65) rotate(138.17)"
-              >
-                <tspan x={0} y={0}>
-                  O
-                </tspan>
-              </text>
-              <text
-                className={cx('typo')}
-                transform="translate(278.62 314.49) rotate(151.7)"
-              >
-                <tspan x={0} y={0}>
-                  R
-                </tspan>
-              </text>
-              <text
-                className={cx('typo')}
-                transform="translate(247.64 330.42) rotate(160.98)"
-              >
-                <tspan x={0} y={0}>
-                  {''}
-                </tspan>
-              </text>
-              <text
-                className={cx('typo')}
-                transform="translate(235.02 335.72) rotate(169.78)"
-              >
-                <tspan x={0} y={0}>
-                  T
-                </tspan>
-              </text>
-              <text
-                className={cx('typo')}
-                transform="translate(203.7 341.65) rotate(-178.2)"
-              >
-                <tspan x={0} y={0}>
-                  E
-                </tspan>
-              </text>
-              <text
-                className={cx('typo')}
-                transform="translate(171.83 341.11) rotate(-165.59)"
-              >
-                <tspan x={0} y={0}>
-                  A
-                </tspan>
-              </text>
-              <text
-                className={cx('typo')}
-                transform="translate(138.35 333.43) rotate(-150.99)"
-              >
-                <tspan x={0} y={0}>
-                  M
-                </tspan>
-              </text>
-              <text
-                className={cx('typo')}
-                transform="translate(101.67 311.82) rotate(-140.34)"
-              >
-                <tspan x={0} y={0}>
-                  {' '}
-                </tspan>
-              </text>
-              <text
-                className={cx('typo')}
-                transform="translate(90.42 304.07) rotate(-130.69)"
-              >
-                <tspan x={0} y={0}>
-                  O
-                </tspan>
-              </text>
-              <text
-                className={cx('typo')}
-                transform="translate(66.22 275.98) rotate(-117.62)"
-              >
-                <tspan x={0} y={0}>
-                  P
-                </tspan>
-              </text>
-              <text
-                className={cx('typo')}
-                transform="translate(51.18 247.89) rotate(-105.43)"
-              >
-                <tspan x={0} y={0}>
-                  E
-                </tspan>
-              </text>
-              <text
-                className={cx('typo')}
-                transform="translate(42.12 217.37) rotate(-92.34)"
-              >
-                <tspan x={0} y={0}>
-                  N
-                </tspan>
-              </text>
-              <text
-                className={cx('typo')}
-                transform="translate(40.71 180.29) rotate(-79.84)"
-              >
-                <tspan x={0} y={0}>
-                  F
-                </tspan>
-              </text>
-              <text
-                className={cx('typo')}
-                transform="translate(45.63 151.37) rotate(-68.6)"
-              >
-                <tspan x={0} y={0}>
-                  L
-                </tspan>
-              </text>
-              <text
-                className={cx('typo')}
-                transform="translate(55.25 124.59) rotate(-56.26)"
-              >
-                <tspan x={0} y={0}>
-                  O
-                </tspan>
-              </text>
-              <text
-                className={cx('typo')}
-                transform="translate(75.49 93.63) rotate(-42.29)"
-              >
-                <tspan x={0} y={0}>
-                  O
-                </tspan>
-              </text>
-              <text
-                className={cx('typo')}
-                transform="translate(102.76 68.56) rotate(-28.75)"
-              >
-                <tspan x={0} y={0}>
-                  R
-                </tspan>
-              </text>
-            </svg>
-          </motion.div>
-        </motion.div> */}
 
         <div className="g-row">
           <div className="col-lg-8 offset-lg-2">
@@ -554,10 +305,19 @@ const IndexPage = () => {
                     'px-xs-1'
                   )}
                 >
-                  <Link to={'.'}>
-                    <h2 className="h2">{item.title}</h2>
-                    <p>{item.desc}</p>
-                  </Link>
+                  <div className={cx('flipBox')}>
+                    <div className={cx('flipBoxInner')}>
+                      {/* box front */}
+                      <div className={cx('flipBoxFront')}>
+                        <h2>{item.title}</h2>
+                      </div>
+                      {/* box back */}
+                      <div className={cx('flipBoxBack')}>
+                        <h2>{item.hoverTitle}</h2>
+                        <p>{item.desc}</p>
+                      </div>
+                    </div>
+                  </div>
                 </li>
               ))}
             </ul>
@@ -647,7 +407,13 @@ const IndexPage = () => {
             <ul className={cx('projectList')}>
               {projectList.map((item, index) => (
                 <li key={index}>
-                  <a href="#">{item.title}</a>
+                  <a href=''>
+                    <img src={item.src} alt={item.title} />
+                  </a>
+                  <div className='mt-3'>
+                    <h3 className='mt-2'>{item.title}</h3>
+                    <p className='text-sm text-gray-4'>{item.desc}</p>
+                  </div>
                 </li>
               ))}
             </ul>
